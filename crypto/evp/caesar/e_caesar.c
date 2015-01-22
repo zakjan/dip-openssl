06e572ee75bb0f7e664a7642dc3b5046f10f941e
Jan Zak <zj@zakjan.cz>
2015-01-22 14:03:30 +0800

    use key from cmdline

--- crypto/evp/caesar/e_caesar.c
+++ crypto/evp/caesar/e_caesar.c
@@ -11,24 +11,35 @@ struct evp_cipher_st {
   int (*init)(EVP_CIPHER_CTX *ctx, const unsigned char *key,
     const unsigned char *iv, int enc); // init key
   int (*do_cipher)(EVP_CIPHER_CTX *ctx, unsigned char *out,
-    const unsigned char *in, size_t inl); // encrypt/decrypt data
+    const unsigned char *in, size_t inl); // encrypt/decrypt cipher_ctx
   int (*cleanup)(EVP_CIPHER_CTX *); // cleanup ctx
   int ctx_size; // how big ctx->cipher_data needs to be
   int (*set_asn1_parameters)(EVP_CIPHER_CTX *, ASN1_TYPE *); // Populate a ASN1_TYPE with parameters
   int (*get_asn1_parameters)(EVP_CIPHER_CTX *, ASN1_TYPE *); // Get parameters from a ASN1_TYPE
   int (*ctrl)(EVP_CIPHER_CTX *, int type, int arg, void *ptr); // Miscellaneous operations
-  void *app_data; // Application data
+  void *app_data; // Application cipher_ctx
 } EVP_CIPHER;
 */
 
 
 
+#define CRYPTO_KEYBYTES 16
+#define CRYPTO_NSECBYTES 0
+#define CRYPTO_NPUBBYTES 12
+#define CRYPTO_ABYTES 16
+
+#define CRYPTO_DEBUG 1
+
+int crypto_aead_encrypt(unsigned char *c, unsigned long long *clen, const unsigned char *m, unsigned long long mlen, const unsigned char *ad, unsigned long long adlen, const unsigned char *nsec, const unsigned char *npub, const unsigned char *k);
+int crypto_aead_decrypt(unsigned char *m, unsigned long long *mlen, unsigned char *nsec, const unsigned char *c, unsigned long long clen, const unsigned char *ad, unsigned long long adlen, const unsigned char *npub, const unsigned char *k);
+
 typedef struct {
-  unsigned char *key;
+  const unsigned char *key;
   unsigned char *nsec;
   unsigned char *npub;
   unsigned char *ad;
-} EVP_CAESAR_KEY;
+  size_t ad_length;
+} EVP_CAESAR_CTX;
 
 static int caesar_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key, const unsigned char *iv, int enc);
 static int caesar_cipher(EVP_CIPHER_CTX *ctx, unsigned char *out, const unsigned char *in, size_t inl);
@@ -38,13 +49,13 @@ static int caesar_ctrl(EVP_CIPHER_CTX *ctx, int type, int arg, void *ptr);
 static const EVP_CIPHER caesar = {
   NID_caesar,
   1,
-  16,
-  16,
-  EVP_CIPH_FLAG_CUSTOM_CIPHER,
+  CRYPTO_KEYBYTES,
+  0,
+  EVP_CIPH_CUSTOM_IV | EVP_CIPH_FLAG_CUSTOM_CIPHER,
   caesar_init_key,
   caesar_cipher,
   caesar_cleanup,
-  sizeof(EVP_CAESAR_KEY),
+  sizeof(EVP_CAESAR_CTX),
   NULL,
   NULL,
   caesar_ctrl,
@@ -55,69 +66,80 @@ const EVP_CIPHER *EVP_caesar(void) {
   return &caesar;
 }
 
-#define CRYPTO_KEYBYTES 16
-#define CRYPTO_NSECBYTES 0
-#define CRYPTO_NPUBBYTES 12
-#define CRYPTO_ABYTES 16
-
-#define CRYPTO_ADBYTES 16
-
 static int caesar_init_key(EVP_CIPHER_CTX *ctx, const unsigned char *key, const unsigned char *iv, int enc) {
+#ifdef CRYPTO_DEBUG
   fprintf(stderr, "caesar_init_key\n");
+#endif
 
-  EVP_CAESAR_KEY *data = (EVP_CAESAR_KEY *)ctx->cipher_data;
+  EVP_CAESAR_CTX *cipher_ctx = (EVP_CAESAR_CTX *)ctx->cipher_data;
 
-  data->key = (unsigned char *)calloc(CRYPTO_KEYBYTES, sizeof(unsigned char));
-  data->nsec = (unsigned char *)calloc(CRYPTO_NSECBYTES, sizeof(unsigned char));
-  data->npub = (unsigned char *)calloc(CRYPTO_NPUBBYTES, sizeof(unsigned char));
-  data->ad = (unsigned char *)calloc(CRYPTO_ADBYTES, sizeof(unsigned char));
-
-  memset(data->key, 0, CRYPTO_KEYBYTES);
-  memset(data->nsec, 0, CRYPTO_NSECBYTES);
-  memset(data->npub, 0, CRYPTO_NPUBBYTES);
-  memset(data->ad, 0, CRYPTO_ADBYTES);
+  cipher_ctx->key = key;
+  cipher_ctx->nsec = (unsigned char *)calloc(CRYPTO_NSECBYTES, sizeof(unsigned char));
+  cipher_ctx->npub = (unsigned char *)calloc(CRYPTO_NPUBBYTES, sizeof(unsigned char));
+  cipher_ctx->ad = (unsigned char *)calloc(1, sizeof(unsigned char));
+  cipher_ctx->ad_length = 0;
 
   return 1;
 }
 
 static int caesar_cipher(EVP_CIPHER_CTX *ctx, unsigned char *out, const unsigned char *in, size_t in_length) {
-  fprintf(stderr, "caesar_cipher: in_length=%d\n", in_length);
+#ifdef CRYPTO_DEBUG
+  fprintf(stderr, "caesar_cipher: in_length=%d\n", (int)in_length);
+#endif
 
-  size_t i;
   int ret = 0;
   int out_length = 0;
-  EVP_CAESAR_KEY *data = (EVP_CAESAR_KEY *)ctx->cipher_data;
+  EVP_CAESAR_CTX *cipher_ctx = (EVP_CAESAR_CTX *)ctx->cipher_data;
 
   if (in == out) {
-    return;
+    // speed benchmark doesn't work
+    return -1;
   }
 
 
   if (in_length > 0) {
-    if (ctx->encrypt) {
-      ret = crypto_aead_encrypt(out, &out_length, in, in_length, data->ad, CRYPTO_ADBYTES, data->nsec, data->npub, data->key);
+    if (out == NULL) {
+      // associated data
+      free(cipher_ctx->ad);
+      cipher_ctx->ad = (unsigned char *)calloc(in_length, sizeof(unsigned char));
+      cipher_ctx->ad_length = in_length;
+      memcpy(cipher_ctx->ad, in, in_length);
     } else {
-      ret = crypto_aead_decrypt(out, &out_length, data->nsec, in, in_length, data->ad, CRYPTO_ADBYTES, data->npub, data->key);
+      // message
+      if (ctx->encrypt) {
+        ret = crypto_aead_encrypt(out, &out_length, in, in_length, cipher_ctx->ad, cipher_ctx->ad_length, cipher_ctx->nsec, cipher_ctx->npub, cipher_ctx->key);
+      } else {
+        ret = crypto_aead_decrypt(out, &out_length, cipher_ctx->nsec, in, in_length, cipher_ctx->ad, cipher_ctx->ad_length, cipher_ctx->npub, cipher_ctx->key);
+      }
     }
   }
 
+
+#ifdef CRYPTO_DEBUG
+  fprintf(stderr, "  out_length=%d ret=%d\n", out_length, ret);
+#endif
 
   return ret == 0 ? out_length : ret;
 }
 
 static int caesar_cleanup(EVP_CIPHER_CTX *ctx) {
+#ifdef CRYPTO_DEBUG
   fprintf(stderr, "caesar_cleanup\n");
+#endif
 
-  EVP_CAESAR_KEY *data = (EVP_CAESAR_KEY *)ctx->cipher_data;
+  EVP_CAESAR_CTX *cipher_ctx = (EVP_CAESAR_CTX *)ctx->cipher_data;
 
-  free(data->key);
-  free(data->nsec);
-  free(data->npub);
-  free(data->ad);
+  free(cipher_ctx->nsec);
+  free(cipher_ctx->npub);
+  free(cipher_ctx->ad);
 
   return 1;
 }
 
 static int caesar_ctrl(EVP_CIPHER_CTX *ctx, int type, int arg, void *ptr) {
+#ifdef CRYPTO_DEBUG
+  fprintf(stderr, "caesar_ctrl: type=%d arg=%d\n", type, arg);
+#endif
+
   return 1;
 }
--- test-enc.sh
+++ test-enc.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env sh
 
+OPENSSL_DIR=apps
 PLAINTEXT="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+KEY=$(openssl rand 16 | xxd -p)
 
 
 echo "$PLAINTEXT"
 echo
-CIPHERTEXT=$(echo -n "$PLAINTEXT" | apps/openssl enc -caesar -pass pass:aaa -nosalt)
+CIPHERTEXT=$(echo -n "$PLAINTEXT" | $OPENSSL_DIR/openssl enc -caesar -K $KEY | xxd -p)
 echo
 echo "$CIPHERTEXT"
-echo -n "$CIPHERTEXT" | xxd -p
 echo
-PLAINTEXT2=$(echo -n "$CIPHERTEXT" | apps/openssl enc -d -caesar -pass pass:aaa -nosalt)
+PLAINTEXT2=$(echo -n "$CIPHERTEXT" | xxd -r -p | $OPENSSL_DIR/openssl enc -d -caesar -K $KEY)
 echo
 echo "$PLAINTEXT2"
 echo
